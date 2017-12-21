0ef01a1f12fee7527418cd1ee723f45dc8a220f0
Ovv <contact@ovv.wtf>
2017-12-21 22:20:32 +0100

    MessageRouter, 401 & general improvments
    - Use slack-sansio MessageRouter
    - Return 401 on failed slack verification token check

--- sirbot/plugins/slack/endpoints.py
+++ sirbot/plugins/slack/endpoints.py
@@ -3,6 +3,7 @@ import logging
 
 from slack.events import Event
 from slack.commands import Command
+from slack.exceptions import FailedVerification
 from aiohttp.web import Response
 
 LOG = logging.getLogger(__name__)
@@ -19,12 +20,15 @@ async def incoming_event(request):
         else:
             return Response(status=500)
 
-    event = Event.from_http(payload, verification_token=slack.verify)
+    try:
+        event = Event.from_http(payload, verification_token=slack.verify)
+    except FailedVerification:
+        return Response(status=401)
 
     if event['type'] == 'message':
-        callbacks = await _incoming_message(event, request)
+        callbacks = list(_incoming_message(event, request))
     else:
-        callbacks = await _incoming_event(event, request)
+        callbacks = list(_incoming_event(event, request))
 
     if callbacks:
         try:
@@ -36,30 +40,28 @@ async def incoming_event(request):
     return Response(status=200)
 
 
-async def _incoming_event(event, request):
+def _incoming_event(event, request):
     LOG.debug('Incoming event: %s', event)
     slack = request.app.plugins['slack']
-    callbacks = list()
-    for callback in slack.routers['router'].dispatch(event):
-        callbacks.append(asyncio.ensure_future(callback(event, request.app)))
-    return callbacks
+    for handler in slack.routers['event'].dispatch(event):
+        yield asyncio.ensure_future(handler(event, request.app))
 
 
-async def _incoming_message(event, request):
+def _incoming_message(event, request):
     slack = request.app.plugins['slack']
     if event.get('bot_id') == slack.bot_id or event.get('message', {}).get('bot_id') == slack.bot_id:
-        return list()
+        return
 
     LOG.debug('Incoming message: %s', event)
-    callbacks = list()
     mention = slack.bot_user_id in event.get('text', '') or event['channel'].startswith('D')
+
     if mention and 'text' in event:
         event['text'] = event['text'].strip('<@{}>'.format(slack.bot_user_id)).strip()
 
-    for func, need_mention in slack.routers['message'].dispatch(event):
-        if not need_mention or mention:
-            callbacks.append(asyncio.ensure_future(func(event, request.app)))
-    return callbacks
+    for handler in slack.routers['message'].dispatch(event):
+        option = slack.handlers_option[handler]
+        if not option['mention'] or mention:
+            yield asyncio.ensure_future(handler(event, request.app))
 
 
 async def incoming_command(request):
@@ -68,20 +70,19 @@ async def incoming_command(request):
 
     try:
         command = Command(payload, verification_token=slack.verify)
-    except Exception as e:
-        LOG.exception(e)
-        raise
+    except FailedVerification:
+        return Response(status=401)
 
-    callbacks = list()
     LOG.debug('Incoming command: %s', command)
+    callbacks = list()
+    for callback in slack.routers['command'].dispatch(command):
+        callbacks.append(asyncio.ensure_future(callback(command, request.app)))
 
-    try:
-        for callback in slack.routers['command'].dispatch(command):
-            callbacks.append(asyncio.ensure_future(callback(command, request.app)))
-        if callbacks:
+    if callbacks:
+        try:
             asyncio.wait(callbacks, return_when=asyncio.ALL_COMPLETED)
-    except Exception as e:
-        LOG.exception(e)
-        return Response(status=500)
-    else:
-        return Response(status=200)
+        except Exception as e:
+            LOG.exception(e)
+            return Response(status=500)
+
+    return Response(status=200)
--- sirbot/plugins/slack/plugin.py
+++ sirbot/plugins/slack/plugin.py
@@ -4,11 +4,10 @@ import asyncio
 
 from slack import methods
 from slack.io.aiohttp import SlackAPI
-from slack.events import Router as EventRouter
+from slack.events import EventRouter, MessageRouter
 from slack.commands import Router as CommandRouter
 
 from . import endpoints
-from .message import MessageRouter
 
 LOG = logging.getLogger(__name__)
 
@@ -22,6 +21,7 @@ class SlackPlugin:
         self.verify = verify or os.environ['SLACK_VERIFY']
         self.bot_id = bot_id or os.environ.get('SLACK_BOT_ID')
         self.bot_user_id = bot_user_id or os.environ.get('SLACK_BOT_USER_ID')
+        self.handlers_option = {}
 
         if not self.bot_user_id and not self.bot_id:
             LOG.warning('`SLACK_BOT_USER_ID` or `SLACK_BOT_ID` required to use `on_mention` routing.')
@@ -41,23 +41,30 @@ class SlackPlugin:
 
         if self.bot_user_id and not self.bot_id:
             sirbot.on_startup.append(self.find_bot_id)
+        elif not self.bot_user_id and not self.bot_id:
+            sirbot.on_startup.append(self.find_bot_user_id)
 
-    def on_event(self, event_type, endpoint):
-        if not asyncio.iscoroutinefunction(endpoint):
-            endpoint = asyncio.coroutine(endpoint)
-        self.routers['event'].register(event_type, endpoint)
+    def on_event(self, event_type, handler):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+        self.routers['event'].register(event_type, handler)
 
-    def on_command(self, command, endpoint):
-        if not asyncio.iscoroutinefunction(endpoint):
-            endpoint = asyncio.coroutine(endpoint)
-        self.routers['command'].register(command, endpoint)
+    def on_command(self, command, handler):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+        self.routers['command'].register(command, handler)
     
-    def on_message(self, pattern, endpoint, flags=0, channel='*', mention=False):
-        if not asyncio.iscoroutinefunction(endpoint):
-            endpoint = asyncio.coroutine(endpoint)
-        self.routers['message'].register(pattern=pattern, endpoint=endpoint, flags=flags, channel=channel,
-                                         mention=mention)
+    def on_message(self, pattern, handler, flags=0, channel='*', mention=False):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+
+        self.handlers_option[handler] = {'mention': mention}
+        self.routers['message'].register(pattern=pattern, handler=handler, flags=flags, channel=channel)
 
     async def find_bot_id(self, app):
         rep = await self.api.query(url=methods.USERS_INFO, data={'user': self.bot_user_id})
         self.bot_id = rep['user']['profile']['bot_id']
+
+    async def find_bot_user_id(self, app):
+        rep = await self.api.query(url=methods.CHAT_POST_MESSAGE, data={'channel': 'general', 'text': 'Looking for bot id'})
+        LOG.warning('The BOT_USER_ID is : "%s"', rep['message']['user'])
